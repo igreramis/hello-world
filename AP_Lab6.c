3518d48f4e8f6db16b3c0ccd217730e821582a3d
anony <anony@gmail.com>
2018-05-27 13:14:25 +0500

    eth_tx()

--- AP_Lab6.c
+++ AP_Lab6.c
@@ -56,6 +56,10 @@ extern NotifyCharacteristic_t NotifyCharacteristicList[];
 #define PAYLOAD_L     2
 #define COMMAND_L     2
 
+void eth_tx(char *buffer)
+{
+  ieee_tx(buffer);
+}
 void ble_rx(char *buffer)
 {
   ti_ble_rx(buffer);
